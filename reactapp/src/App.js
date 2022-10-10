62c7b02a616f64da02594adf84e42fd4588e88d5
sriram <sriramachandran28@gmail.com>
2022-10-10 12:01:31 +0530

    login form added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Rubik:400,700'/>
+
+            <body>
+        <div className="login-form">
+          <form action="" method="POST">
+            <h1>Login</h1>
+            <div className="content">
+              <div className="input-field">
+                <input type="email" placeholder="Email" autocomplete="nope"/>
+              </div>
+              <div className="input-field">
+                <input type="password" placeholder="Password" autocomplete="new-password"/>
+              </div>
+              <a href="#" className="link">Forgot Your Password?</a>
+            </div>
+            <div className="action">
+              <button>Register</button>
+              <button type="submit">Sign in</button>
+            </div>
+          </form>
+        </div>
+
+        </body>
+
+
       </header>
     </div>
   );
